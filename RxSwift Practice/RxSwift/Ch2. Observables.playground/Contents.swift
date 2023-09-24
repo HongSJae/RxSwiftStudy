39d5ca49ca111d918491bb25ae932f75eea2f5de
hsj._.06 <gtw030488@gmail.com>
2023-09-24 14:46:09 +0900

    ch1 - Observable 구독

--- RxSwift Practice/RxSwift/Ch2. Observables.playground/Contents.swift
+++ RxSwift Practice/RxSwift/Ch2. Observables.playground/Contents.swift
@@ -25,6 +25,59 @@ example(of: "just, of, from") {
     
 }
 
-example(of: "just, of, from") {
-    <#code#>
+// MARK: - Observable 구독
+example(of: "subscribe") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable = Observable.of(one, two, three)
+    observable.subscribe { event in
+        print(event)
+    }
+    observable.subscribe { event in
+        if let event = event.element {
+            print(event)
+        }
+    }
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
+}
+
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable.subscribe(
+        onNext: { element in
+            print(element)
+        },
+        onCompleted: {
+            print("Completed!")
+        }
+    )
+}
+
+example(of: "never") {
+    let observable = Observable<Void>.never()
+    
+    observable.subscribe(
+        onNext: { element in
+            print(element)
+        },
+        onCompleted: {
+            print("Completed!")
+        }
+    )
+}
+
+example(of: "range") {
+    let observable = Observable<Int>.range(start: 1, count: 10)
+    observable.subscribe(onNext: { i in
+        let n = Double(i)
+        let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, n)) / 2.23606).rounded())
+        print(fibonacci)
+    })
+}
+
 }
